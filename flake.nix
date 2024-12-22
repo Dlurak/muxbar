71a77512dddca3d27b517fdb954c0057f714c1bd
lordkekz <lordkekz@lkekz.de>
2024-12-22 02:15:38 +0100

    Add Nix Flake

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    naersk.url = "github:nix-community/naersk";
+    systems.url = "github:nix-systems/default-linux";
+    devenv.url = "github:cachix/devenv";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
+  };
+
+  nixConfig = {
+    #extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    #extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs @ {
+    flake-parts,
+    systems,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.devenv.flakeModule];
+      systems = import systems;
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+        packages = rec {
+          default = muxbar;
+          muxbar = inputs.naersk.lib.${system}.buildPackage {src = ./.;};
+        };
+
+        devenv.shells.default = {
+          name = "muxbar";
+          languages.rust.enable = true;
+        };
+
+        formatter = pkgs.alejandra;
+      };
+      flake = {};
+    };
+}
