f870a8646c102a11fc7344e60688878b2b0bbad5
Dronakurl <konrad2710@gmail.com>
2024-12-30 20:36:14 +0100

    finish high cpu module

--- src/modules/high_cpu.rs
+++ src/modules/high_cpu.rs
@@ -1,10 +1,13 @@
 use super::Module;
 use crate::colors::{Color, Style};
 use crate::icons::Icon;
+use std::collections::HashMap;
 use std::fmt;
-use sysinfo::System;
+use std::process::Command;
 
+/// A module to display the process with the highest CPU usage.
 pub struct HighCpuModule {
+    /// Field to store process information
     process_info: String,
 }
 
@@ -15,36 +18,53 @@ impl fmt::Display for HighCpuModule {
 }
 
 impl HighCpuModule {
+    /// Creates a new instance of HighCpuModule.
+    ///
+    /// This method runs the `ps` command to get the process information,
+    /// processes the output to find the process with the highest CPU usage,
+    /// and returns a boxed Module containing the HighCpuModule.
+    ///
+    /// # Returns
+    ///
+    /// A `Box<Module<HighCpuModule>>` containing the process information.
     pub fn new() -> Box<Module<HighCpuModule>> {
-        let mut system = System::new_all();
-        system.refresh_all();
+        let output = Command::new("ps")
+            .arg("axo")
+            .arg("pid,pcpu,comm")
+            .output()
+            .expect("Failed to execute ps command");
 
-        let process_info: Vec<String> = system
-            .processes()
-            .values()
-            .filter(|p| p.cpu_usage() > 0.001)
-            // .collect::<Vec<_>>()
-            // .max_by_key(|p| (p.cpu_usage() * 100.0) as u32)
-            .map(|p| {
-                let name = p.name().to_str().unwrap_or("").to_string();
-                println!("{:?} {:?}", p.name(), p.cpu_usage());
-                if name.len() > 30 {
-                    format!("{}...", &name[..27])
-                } else {
-                    format!("{} {}", name, p.cpu_usage())
+        let process_info = if output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            let mut process_map: HashMap<String, f32> = HashMap::new();
+
+            stdout.lines().skip(1).for_each(|line| {
+                let parts: Vec<&str> = line.split_whitespace().collect();
+                if let (Some(cpu_usage), Some(name)) = (parts.get(1), parts.get(2)) {
+                    let cpu_usage: f32 = cpu_usage.parse().unwrap_or(0.0);
+                    if cpu_usage > 0.001 && *name != "ps" {
+                        *process_map.entry(name.to_string()).or_insert(0.0) += cpu_usage;
+                    }
                 }
-            })
-            .collect();
-        // println!("{}", process_info);
-        // .unwrap_or_else(|| "No process found".to_string());
+            });
 
-        // #[cfg(debug_assertions)]
-        // println!("{:?}", process_info);
+            process_map
+                .into_iter()
+                .max_by_key(|&(_, cpu_usage)| (cpu_usage * 100.0) as i32)
+                .map(|(name, cpu_usage)| {
+                    if name.len() > 30 {
+                        format!("{}... {:.2}%", &name[..27], cpu_usage)
+                    } else {
+                        format!("{} {:.2}%", name, cpu_usage)
+                    }
+                })
+                .unwrap_or_else(|| "No high CPU process found".to_string())
+        } else {
+            "Failed to get process info".to_string()
+        };
 
         Box::new(Module::new(
-            HighCpuModule {
-                process_info: "porces".to_string(),
-            },
+            HighCpuModule { process_info },
             Some(Icon::Manual("󰑓")),
             Style {
                 fg: Color::Any("color61"),
