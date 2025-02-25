8cc28e707f1280283a117e8ddaf74cb9512c6f4b
Dlurak <84224239+Dlurak@users.noreply.github.com>
2025-02-25 13:21:27 +0100

    Solve a small bug regarding time rerenders

--- modules/src/modules/datetime.rs
+++ modules/src/modules/datetime.rs
@@ -6,7 +6,7 @@ use crate::{
 use chrono::Local;
 use std::{
     fmt,
-    time::{Duration, Instant},
+    time::{Duration, Instant, SystemTime, UNIX_EPOCH},
 };
 
 #[derive(Clone, Copy)]
@@ -42,7 +42,14 @@ impl ToModule for DateTime<'_> {
         Style::new_with_fg(Color::Magenta)
     }
     fn next_render_time(&self) -> Option<Instant> {
-        // TODO: Calculate the actual needed instant
-        Some(Instant::now() + Duration::from_secs(1))
+        let now_sys = SystemTime::now();
+        let now_ins = Instant::now();
+        let duration_since_epoch = now_sys.duration_since(UNIX_EPOCH).unwrap();
+        let duration_since_epoch = Duration::from_secs(duration_since_epoch.as_secs());
+
+        let now_instant_duration = now_ins.duration_since(now_ins - duration_since_epoch);
+        let truncated = now_ins - (now_instant_duration - duration_since_epoch);
+
+        Some(truncated + Duration::from_secs(1))
     }
 }
